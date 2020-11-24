05ed26c9653fff92df4d015daca8d2d9e7cf37cb
Melanie Rush <merush@microsoft.com>
2020-11-23 21:32:10 -0700

    removing retweets

--- app.py
+++ app.py
@@ -12,17 +12,19 @@ def get_tweets(keyword, num_tweets, date):
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     api = tweepy.API(auth)
 
-    scrappedTweets = []
+    noRetweetsScrape = []
     # iterate through tweets and save them to array
     for tweet in tweepy.Cursor(api.search, since=date, q=keyword).items(num_tweets):
-
         data = [tweet.created_at, tweet.id, tweet.geo, tweet.text, tweet.user._json['screen_name']]
         data = tuple(data)
-        scrappedTweets.append(data)
+    
+        # We only want data that doesn't have retweets
+        if (not tweet.retweeted) and ('RT @' not in tweet.text):
+            noRetweetsScrape.append(data)
 
     # export tweets to dataframe 
-    df = pd.DataFrame(scrappedTweets, columns = ['created_at', 'tweet_id', 'geo','tweet_text', 'screen_name'])
-    filename = keyword+'_scrape_'+(datetime.datetime.now().strftime("%Y-%m-%d-%H"))+'.csv'
+    df = pd.DataFrame(noRetweetsScrape, columns = ['created_at', 'tweet_id', 'geo','tweet_text', 'screen_name'])
+    filename = keyword+'_scrape_'+(datetime.datetime.now().strftime("%Y-%m-%d"))+'.csv'
     df.to_csv(filename, index=False)
 
 # clean tweets
@@ -33,4 +35,4 @@ def clean_tweets(cleanTweets):
 def get_sentiment(sentiment):
     return sentiment
 
-get_tweets("rollerskating", 10, '2020-02-28')
+get_tweets("rollerskating", 50000, '2020-02-28')
